c1155bf6d636918814c043e4381cb41b68d4495b
hadassaby <78303045+hadassaby@users.noreply.github.com>
2021-04-10 23:20:28 +0300

    Update javascript with signature as svg

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 {
-var wrapper = document.getElementById("signature-pad");
-var clearButton = wrapper.querySelector("[data-action=clear]");
-var savePNGButton = wrapper.querySelector("[data-action=save-png]");
-var canvas = wrapper.querySelector("canvas");
-var signatureDate = document.getElementById("signatureDate");
-var signatureName = document.getElementById("signatureName");
-var signatureLocation = document.getElementById("signatureLocation");
-var signaturePad = new SignaturePad(canvas);
+  import { jsPDF } from "jspdf";
+
+  var wrapper = document.getElementById("signature-pad");
+  var clearButton = wrapper.querySelector("[data-action=clear]");
+  // var savePNGButton = wrapper.querySelector("[data-action=save-png]");
+  var saveSVGButton = wrapper.querySelector("[data-action=save-svg]");
+  var canvas = wrapper.querySelector("canvas");
+
+  var signatureDate = document.getElementById("signatureDate");
+  var signatureName = document.getElementById("signatureName");
+  var signature = document.getElementById("signature");
+
+  var signature_year = document.getElementById("signature_year");
+  var signature_month = document.getElementById("signature_month");
+  var signature_day = document.getElementById("signature_day");
+  var signature_hour = document.getElementById("signature_hour");
+  var signature_minute = document.getElementById("signature_minute");
+
+  var submitButton = document.getElementById("submitButton");
+
+  var signaturePad = new SignaturePad(canvas);
 
 //var signaturePad = new SignaturePad(canvas, {
   // It's Necessary to use an opaque color when saving image as JPEG;
@@ -17,91 +30,146 @@ var signaturePad = new SignaturePad(canvas);
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
 // This also causes canvas to be cleared.
-function resizeCanvas() {
-  if (signaturePad.isEmpty() == false)
-  {
-    return;
+  function resizeCanvas() {
+    if (signaturePad.isEmpty() == false)
+    {
+      return;
+    }
+
+    // When zoomed out to less than 100%, for some very strange reason,
+    // some browsers report devicePixelRatio as less than 1
+    // and only part of the canvas is cleared then.
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+
+    // This part causes the canvas to be cleared
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+
+    // This library does not listen for canvas changes, so after the canvas is automatically
+    // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+    // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+    // that the state of this library is consistent with visual state of the canvas, you
+    // have to clear it manually.
+    signaturePad.clear();
   }
 
-  // When zoomed out to less than 100%, for some very strange reason,
-  // some browsers report devicePixelRatio as less than 1
-  // and only part of the canvas is cleared then.
-  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+  // On mobile devices it might make more sense to listen to orientation change,
+  // rather than window resize events.
+  window.onresize = resizeCanvas;
+  resizeCanvas();
 
-  // This part causes the canvas to be cleared
-  canvas.width = canvas.offsetWidth * ratio;
-  canvas.height = canvas.offsetHeight * ratio;
-  canvas.getContext("2d").scale(ratio, ratio);
+  function download(dataURL, filename) {
+    if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
+      window.open(dataURL);
+    } else {
+      var blob = dataURLToBlob(dataURL);
+      var url = window.URL.createObjectURL(blob);
 
-  // This library does not listen for canvas changes, so after the canvas is automatically
-  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-  // that the state of this library is consistent with visual state of the canvas, you
-  // have to clear it manually.
-  signaturePad.clear();
-}
+      var a = document.createElement("a");
+      a.style = "display: none";
+      a.href = url;
+      a.download = filename;
 
-// On mobile devices it might make more sense to listen to orientation change,
-// rather than window resize events.
-window.onresize = resizeCanvas;
-resizeCanvas();
+      document.body.appendChild(a);
+      a.click();
 
-function download(dataURL, filename) {
-  if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
-    window.open(dataURL);
-  } else {
-    var blob = dataURLToBlob(dataURL);
-    var url = window.URL.createObjectURL(blob);
-
-    var a = document.createElement("a");
-    a.style = "display: none";
-    a.href = url;
-    a.download = filename;
-
-    document.body.appendChild(a);
-    a.click();
-
-    window.URL.revokeObjectURL(url);
-  }
-}
-
-// One could simply use Canvas#toBlob method instead, but it's just to show
-// that it can be done using result of SignaturePad#toDataURL.
-function dataURLToBlob(dataURL) {
-  // Code taken from https://github.com/ebidel/filer.js
-  var parts = dataURL.split(';base64,');
-  var contentType = parts[0].split(":")[1];
-  var raw = window.atob(parts[1]);
-  var rawLength = raw.length;
-  var uInt8Array = new Uint8Array(rawLength);
-
-  for (var i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i);
+      window.URL.revokeObjectURL(url);
+    }
   }
 
-  return new Blob([uInt8Array], { type: contentType });
-}
+  // One could simply use Canvas#toBlob method instead, but it's just to show
+  // that it can be done using result of SignaturePad#toDataURL.
+  function dataURLToBlob(dataURL) {
+    // Code taken from https://github.com/ebidel/filer.js
+    var parts = dataURL.split(';base64,');
+    var contentType = parts[0].split(":")[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+    var uInt8Array = new Uint8Array(rawLength);
 
-clearButton.addEventListener("click", function (event) {
-  signaturePad.clear();
-});
+    for (var i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
 
-savePNGButton.addEventListener("click", function (event) {
-  if (signaturePad.isEmpty()) {
-    alert("נא לחתום במסגרת");
-  } else {
-    var dataURL = signaturePad.toDataURL();
+    return new Blob([uInt8Array], { type: contentType });
+  }
 
-    // update signature date value and make it readonly
-    signatureDate.contentEditable = true;
-    var d = new Date();
-    var dateTimeLocalValue = (new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString()).slice(0, -1);
-    signatureDate.value = dateTimeLocalValue;
-    signatureDate.contentEditable = false;
+  clearButton.addEventListener("click", function (event) {
+    signaturePad.clear();
+  });
+
+  saveSVGButton.addEventListener("click", saveSignature);
+
+  submitButton.addEventListener("click", submitForm);
+
+  function saveSignature() {
+    if (signaturePad.isEmpty()) {
+      alert("נא לחתום במסגרת לפני שמירה");
+    } else {
+      var dataURL = signaturePad.toDataURL('image/svg+xml');
+  
+      // update signature date value and make it readonly
+      signatureDate.contentEditable = true;
+      signature_year.contentEditable = true;
+      signature_month.contentEditable = true;
+      signature_day.contentEditable = true;
+      signature_hour.contentEditable = true;
+      signature_minute.contentEditable = true;
+
+      var d = new Date();
+      var dateTimeLocal = new Date(d.getTime() - d.getTimezoneOffset() * 60000);
+      var dateTimeLocalValue = (dateTimeLocal.toISOString()).slice(0, -1);
+      signatureDate.value = dateTimeLocalValue;
+
+      signature_year.value = dateTimeLocal.getFullYear();
+      signature_month.value = dateTimeLocal.getMonth();
+      signature_day.value = dateTimeLocal.getDay();
+      signature_hour.value = dateTimeLocal.getHours();
+      signature_minute.value = dateTimeLocal.getMinutes();
+      
+      signatureDate.contentEditable = false;
+      signature_year.contentEditable = false;
+      signature_month.contentEditable = false;
+      signature_day.contentEditable = false;
+      signature_hour.contentEditable = false;
+      signature_minute.contentEditable = false;
+
+      // set signature location value
+      signature.contentEditable = true;
+      signature.value = dataURL;
+      signature.contentEditable = false;
+      // download(dataURL, "signature.png");
+    }
+  }
+
+  function validateForm() {
+    if (!signatureData.value)
+    {
+      alert("נא לחתום במסגרת ולשמור לפני שליחה");
+      return false;
+    }
+    return true;
+  }
+  
+  function submitForm() {
+    var doc = new jsPDF();          
+    var elementHandler = {
+      '#ignorePDF': function (element, renderer) {
+        return true;
+      }
+    };
+    var source = window.document.getElementsByTagName("body")[0];
+    doc.fromHTML(
+        source,
+        15,
+        15,
+        {
+          'width': 180,'elementHandlers': elementHandler
+        });
     
-    // set signature location value
-    signatureLocation.value = dataURL;
-    // download(dataURL, "signature.png");
+    doc.output("dataurlnewwindow");
   }
-});
+
 }
+
